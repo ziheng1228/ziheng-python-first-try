6542718ef5f4010dbf0b49ec50e263e636527942
YANG ZIHENG <396090037@qq.com>
2018-08-03 12:07:09 +0800

    beautify front-end

---
+++ www/config_default.py
@@ -0,0 +1,18 @@
+'''
+Default configurations.
+'''
+
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'ziheng',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
